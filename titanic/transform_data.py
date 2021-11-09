74a6323abfc35c26c96bfb19e9dc45db16917a9e
pallavi-pannu-oc <67274173+pallavi-pannu-oc@users.noreply.github.com>
2021-11-09 14:51:14 +0530

    Update transform_data.py

--- titanic/transform_data.py
+++ titanic/transform_data.py
@@ -5,7 +5,6 @@ class Transformer():
         data_to_preprocess = data_to_preprocess.fillna(data_to_preprocess.mean())
         features = ["Pclass", "Sex", "SibSp", "Parch"]
         train_df = pd.get_dummies(data_to_preprocess[features])
-        data_to_preprocess = pd.concat([data_to_preprocess[["Age", "Fare", "Survived", "PassengerId"]], train_df], axis=1)
-        data_to_preprocess = data_to_preprocess.drop(["PassengerId","Survived"], 1).values
+        data_to_preprocess = pd.concat([data_to_preprocess[["Age", "Fare", "Survived", "PassengerId","timestamp"]], train_df], axis=1)
         preprocessed_data = data_to_preprocess
         return preprocessed_data
